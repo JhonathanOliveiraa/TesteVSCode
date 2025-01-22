d94e06b85c8ea206d06420a08a7b15d7395f57a9
Jhonathan Almeida <jhoneoliveiratetris@gmail.com>
2025-01-21 22:07:50 -0300

    Quantidade de litros

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    static void Main(string[] args)
+    {
+        CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+        int tempoViagem = int.Parse(console.WriteLine());
+        int velocidadeMedia = int.Parse(console.WriteLine());
+        double distancia = velocidadeMedia * tempoViagem;
+        double quantidadeLitros = distancia / 12;
+
+        Console.WriteLine($"{quantidadeLitros}");
+    }
+}
